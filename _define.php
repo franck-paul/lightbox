b82a5ebb6da5d90d8b29ad2c423023b4abf72a66
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:16 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,21 +10,25 @@
  * @copyright Olivier Meunier
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "lightBox",                                          // Name
-    "lightBox like effect on images using jquery modal", // Description
-    "Olivier Meunier and contributors",                  // Author
+    'lightBox',                                          // Name
+    'lightBox like effect on images using jquery modal', // Description
+    'Olivier Meunier and contributors',                  // Author
     '1.5',                                               // Version
     [
         'requires'    => [['core', '2.17']],                        // Dependencies
         'permissions' => 'admin',                                   // Permissions
         'type'        => 'plugin',                                  // Type
-        'support'     => 'https://github.com/franck-paul/lightbox', // Support URL
         'settings'    => [
             'blog' => '#params.lightbox'
-        ]
+        ],
+
+        'details'    => 'https://open-time.net/?q=lightbox',       // Details URL
+        'support'    => 'https://github.com/franck-paul/lightbox', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/lightbox/main/dcstore.xml'
     ]
 );
