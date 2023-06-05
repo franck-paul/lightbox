7d9dbce6268a91404ef7770d8811256d4ed1822a
franck <carnet.franck.paul@gmail.com>
2023-06-05 19:12:24 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -11,7 +11,7 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['lightBox'] = '';
+L10n::$locales['lightBox'] = 'lightBox';
 L10n::$locales['lightBox like effect on images using jquery modal'] = 'Effets lightbox-like sur les images';
-L10n::$locales['LightBox'] = 'Activer lightBox';
+L10n::$locales['LightBox'] = 'LightBox';
 L10n::$locales['Enable lightBox'] = 'Activer lightBox';
