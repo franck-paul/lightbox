74743a746bda7c55f956cbd443e0dd51674a7f1c
franck <carnet.franck.paul@gmail.com>
2023-06-12 10:20:52 +0200

    Don't do things in frontend if plugin disabled

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'lightBox',
     'lightBox like effect on images using jquery modal',
     'Olivier Meunier and contributors',
-    '3.0.1',
+    '3.0.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
