a9e2150b8460e3428b390a8f8a06275b9df4a089
franck <carnet.franck.paul@gmail.com>
2024-12-25 09:47:13 +0100

    JS Code review (WIP)

--- js/public.js
+++ js/public.js
@@ -1,17 +1,17 @@
 /*global $, dotclear */
 'use strict';
 
-$(() => {
+dotclear.ready(() => {
   const lb = dotclear.getData('lightbox');
   const lb_settings = {
-    'loader_img' : `${lb.url}/img/loader.gif`,
-    'prev_img'   : `${lb.url}/img/prev.png`,
-    'next_img'   : `${lb.url}/img/next.png`,
-    'close_img'  : `${lb.url}/img/close.png`,
-    'blank_img'  : `${lb.url}/img/blank.gif`
+    loader_img: `${lb.url}/img/loader.gif`,
+    prev_img: `${lb.url}/img/prev.png`,
+    next_img: `${lb.url}/img/next.png`,
+    close_img: `${lb.url}/img/close.png`,
+    blank_img: `${lb.url}/img/blank.gif`,
   };
-  const sel = lb.extensions.map(x => `a[href$=".${x}"], a[href$=".${x.toUpperCase()}"]`).join(', ');
-  $('div.post, article.post').each(function() {
+  const sel = lb.extensions.map((x) => `a[href$=".${x}"], a[href$=".${x.toUpperCase()}"]`).join(', ');
+  $('div.post, article.post').each(function () {
     $(this).find(sel).modalImages(lb_settings);
   });
 });
