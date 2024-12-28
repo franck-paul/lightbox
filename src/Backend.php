a2cd59445bbf908967e660ebddb46f76807f3e88
franck <carnet.franck.paul@gmail.com>
2024-12-28 05:51:02 +0100

    PHP Code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief lightbox, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('lightBox') . __('lightBox like effect on images using jquery modal');
+        __('lightBox');
+        __('lightBox like effect on images using jquery modal');
 
         return self::status(My::checkContext(My::BACKEND));
     }
