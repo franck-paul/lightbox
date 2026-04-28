5502ef65bfbba8ccf42ba156cc09c3122d2ba3fe
franck <contact@open-time.net>
2026-04-28 14:29:54 +0200

    Harmonize usage of App::blogWorkspace()::NS_... and App::userWorkspace()::WS_... (and use short version of constant name for types)

--- src/BackendBehaviors.php
+++ src/BackendBehaviors.php
@@ -15,6 +15,7 @@ declare(strict_types=1);
 
 namespace Dotclear\Plugin\lightbox;
 
+use Dotclear\App;
 use Dotclear\Helper\Html\Form\Checkbox;
 use Dotclear\Helper\Html\Form\Fieldset;
 use Dotclear\Helper\Html\Form\Label;
@@ -42,7 +43,7 @@ class BackendBehaviors
 
     public static function adminBeforeBlogSettingsUpdate(): string
     {
-        My::settings()->put('enabled', !empty($_POST['lightbox_enabled']), 'boolean');
+        My::settings()->put('enabled', !empty($_POST['lightbox_enabled']), App::blogWorkspace()::NS_BOOL);
 
         return '';
     }
